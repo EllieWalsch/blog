9ab4338aeb527d1f388a4e4bbdaf92985d297489
EllieWalsch <elliewalsch4@gmail.com>
2023-01-24 22:27:57 -0600

    Inform Express.js which template engine to use

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routes = require('./controllers/homeroutes');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +11,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
