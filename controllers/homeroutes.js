b3f56018c3295e0e4cb8493e7281008854b729fb
EllieWalsch <elliewalsch4@gmail.com>
2023-01-24 22:26:10 -0600

    Render homepage using all posts

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -2,19 +2,14 @@ const router = require('express').Router();
 const { Post, User } = require('../models');
 
 router.get('/', async (_, res) => {
-  try {
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-    res.status(200).json(postData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  const postData = await Post.findAll({
+    include: [ User ],
+  });
+
+  const posts = postData.map((post) => post.get({ plain: true }));
+  console.log(posts);
+  res.render('homepage', { posts });
+
   // get all posts
 });
 
@@ -28,7 +23,7 @@ router.get('/post/:id', async (req, res) => {
         },
       ],
     });
-    res.status(200).json(postData)
+    res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
