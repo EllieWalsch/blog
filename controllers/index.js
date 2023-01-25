aa9fd41d7ead8289e03a6cbc1472c3638a52ad55
EllieWalsch <elliewalsch4@gmail.com>
2023-01-25 13:14:35 -0600

    WIP add login feature

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
+const apiRoutes = require('./api');
 const homeRoutes = require('./homeroutes');
 
 // http://localhost:3000/ is a HTML route - loads webpage
 router.use('/', homeRoutes);
+
+// api route - CRUD
+router.use('/api', apiRoutes);
 
 module.exports = router;
